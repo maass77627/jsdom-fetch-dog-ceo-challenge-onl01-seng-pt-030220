581a51fdecd68331480fab573d20d901242eb6d4
kelly r maass <kmaass77627@gmail.com>
2020-09-25 12:45:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ document.addEventListener("DOMContentLoaded", function(){
     })
     .then(function(json){
       console.log(json)
+      picsOnDom(json)
     })
-
-
 })
+
+function picsOnDom(pictures) {
+//for (i = 0; i < json.length; i++)
+  //json[i]
+  const ul = document.getElementById("dog-breeds")
+  //let li = document.createElement("li")
+  pictures.forEach(function(picture) {
+    let li = document.createElement("li")
+    li.innerHTML = picture
+    ul.appendChild("li")  
+});
+  
+}
