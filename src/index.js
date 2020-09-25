7a6000e68716116af8ab3852d51e02735c24928d
kelly r maass <kmaass77627@gmail.com>
2020-09-25 12:14:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json)
+    const pics = []
+    pics.push(json)
+    addImages(pics)
+  })
+
+  function addImages(pics) {
+    const list = document.getElementById('dog-breeds')
+    pics.forEach(pic => {
+      let item = document.createElement('li')
+      item.innerHTML = pic
+      list.appendChild(item)
+
+    })
+
+  }
