7e25b84545d64f287dc2243921eb980a860eb340
kelly r maass <kmaass77627@gmail.com>
2020-09-25 17:52:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -59,7 +59,8 @@ function challengeFour(breeds) {
   }
       }
     })
-  }
+  })
+})
     // for (x in breeds) {
     //   let liThree = document.createElement("li")
     //   liThree.innerHTML = breeds[x]
