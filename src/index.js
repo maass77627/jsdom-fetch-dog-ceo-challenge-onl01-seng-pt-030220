8333eab2da8671ccc3abccfffc7d755e9b045ad8
kelly r maass <kmaass77627@gmail.com>
2020-09-25 12:22:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(function(response){
+      response.json
+    })
+    .then(function(json){
+      console.log(json)
+    })
 
 
 })
