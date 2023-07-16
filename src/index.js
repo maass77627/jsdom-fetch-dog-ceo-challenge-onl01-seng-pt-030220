83862c6dd07e85c7a7298155ee37176ba8c8675d
kelly_maass <kmaass77627@gmail.com>
2023-07-15 20:46:55 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,66 +1,94 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-
-  fetch("https://dog.ceo/api/breeds/image/random/4")
-    .then(function(response){
-      return response.json()
-    })
-    .then(function(json){
-      console.log(json)
-      picsOnDom(json.message)
-});
-    })
-
-function picsOnDom(doggys) {
-const ul = document.getElementById("dog-image-container")
-    for (const picture of doggys) {
-    let image = document.createElement("img")
-    image.src = picture
-    ul.appendChild(image)
-};
-
-fetch('https://dog.ceo/api/breeds/list/all')
+fetch("https://dog.ceo/api/breeds/image/random/4")
 .then(function(response){
   return response.json()
 })
 .then(function(json){
-//console.log(json)
-let newjson = Object.keys(json.message)
-breedsDom(newjson)
-})
+  //console.log(json)
+  let dogs = json.message
+  dogsondom(dogs)
+}) 
 
-function breedsDom(breeds) {
-  //challengeFour();
-const ulTwo = document.getElementById("dog-breeds")
-for (x in breeds) {
-  let liTwo = document.createElement("li")
-  liTwo.innerHTML = breeds[x]
-  ulTwo.appendChild(liTwo)
-  liTwo.addEventListener("click", function(event){
-    event.target.style.color = "blue";
-  })
-}
-challengeFour(breeds)
-}
-
-function challengeFour(breeds) {
-  let ulTwo = document.getElementById("dog-breeds")
-  let array = []
-  let selector = document.getElementById("breed-dropdown")
-
-  selector.addEventListener("change", function(){
-      if (selector.value === "a") {
-          ulTwo.innerHTML = "";
-    for (i = 0; i < breeds.length; i++) {
-    if (breeds[i].startsWith("a")) {
-      //array.push(breeds[i])
-      let newli = document.createElement("li")
-      newli.innerHTML = breeds[i]
-      ulTwo.appendChild(newli)
+function dogsondom(dogs) {
+  const ul = document.getElementById("dog-image-container")
+    for (const dog of dogs) {
+      //console.log(dog)
+      let image = document.createElement("img")
+      image.src = dog
+      ul.appendChild(image)
     }
-   }
- }
- });
- }
+}
 
- }
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(function(response){
+        return response.json()
+      })
+      .then(function(json){
+        //console.log(json)
+        let breeds = json.message
+        let breed = Object.keys(breeds)
+        breedsondom(breed)
+      })
+
+
+    function breedsondom(breed) {
+      const ulTwo = document.getElementById("dog-breeds")
+        for(const pup of breed){
+            let liTwo = document.createElement("li")
+            liTwo.innerHTML = pup
+            ulTwo.appendChild(liTwo)
+            liTwo.addEventListener("click", function(e){
+              console.log(e)
+              e.target.style.color = "blue"
+            })
+        }
+        filterbreeds(breed)
+    }
+
+    function filterbreeds(breed) {
+      let selector = document.getElementById("breed-dropdown")
+      selector.addEventListener("change", function(e){
+        let values = e.target.value
+       
+        switch (values) {
+          case values = "a":
+                let breedtwo = breed.filter(dog => dog.charAt(0) == "a")
+                filter(breedtwo)
+                break
+          case values = "b":
+            let breedthree = breed.filter(dog => dog.charAt(0) == "b")
+            filter(breedthree)
+            break
+          case values = "c":
+            let breedfour = breed.filter(dog => dog.charAt(0) == "c")
+            filter(breedfour)
+            break
+          case values = "d":
+            let breedfive = breed.filter(dog => dog.charAt(0) == "d")
+            filter(breedfive)
+            break
+        }
+
+      })
+      }
+         function filter(array) {
+          console.log(array)
+          const list = document.getElementById("dog-breeds")
+          list.innerHTML = ""
+          for (const element of array) {
+            let listitem = document.createElement("li")
+            listitem.innerHTML = element
+            list.appendChild(listitem)
+          }
+         }
+  
+  })
+
+
+
+
+
+
+
+
